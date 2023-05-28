26417080c8318ea351ba34ca1db855731efeed6a
GOLDisGreat <suyogdhungana17@gmail.com>
2023-05-28 19:49:56 +0545

    second commit

--- include/Graph.hpp
+++ include/Graph.hpp
@@ -3,4 +3,24 @@
 
 #include <iostream>
 
+struct Vertex
+{
+};
+
+class Graph
+{
+private:
+    int numVertex;
+
+public:
+    Graph();
+    ~Graph();
+
+    bool isEmpty();
+    bool isDirected();
+
+    void addVertex(Vertex newVertex);
+    void addEdge(Vertex vertex1, Vertex vertex2);
+};
+
 #endif
